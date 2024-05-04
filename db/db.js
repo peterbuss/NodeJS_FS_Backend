650fc345ff019f9e5c4c2c582a03cda49a4cc9a1
Peter BUSS <peter.buss@hispeed.ch>
2024-05-04 17:31:00 +0200

    Updated Router, New User Router, DB functionality

--- db/db.js
+++ db/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
+const User = require('../models/userModel');
 
 const connect = async() => {
     await mongoose.connect(process.env.mongo);
@@ -11,5 +12,18 @@ const connect = async() => {
     */
 };
 
-module.exports = { connect };
+const disconnect = async () => {
+    await mongoose.connection.close();
+};
+
+// obj { firstName: req.body.firstName, email: req.body.email }
+const findUser = async (obj) =>  {
+    User.findOne(obj);
+};
+
+const saveUser = async (newUser) =>  {
+    return await newUser.save();
+};
+
+module.exports = { connect, disconnect, findUser, saveUser };
 
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,66 +1,22 @@
 const express = require('express');
+
 const router = express.Router();
 
-// http://localhost:3001/users
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Successful - GET',
-        metadata: {
-            hostname: req.hostname,
-            method: req.method,
-        },
-    });
+router.post('/register', (req, res, next) => {
+    // findUser
+    // if user exists
+    // return response that says email exists try loging in
+    // else
+    // encrypt the password
+    // set the password with the encryted password
+    // save the user to the database
+
+
 });
 
-// http://localhost:3001/users/34
-router.get('/:id', (req, res, next) => {
-    res.status(200).json({
-        message: 'Successful - GET by ID',
-        metadata: {
-            id: req.params.id,
-            hostname: req.hostname,
-            method: req.method,
-        },
-    });
-});
+router.post('/login', (req, res) => {
 
-// http://localhost:3001/users
-router.post('/', (req, res, next) => {
-    const name = req.body.name;
-    res.status(201).json({
-        message: 'Successful - POST',
-        metadata: {
-            name: name,
-            hostname: req.hostname,
-            method: req.method,
-        },
-    });
 });
-
-// http://localhost:3001/users/34
-router.put('/:id', (req, res, next) => {
-    res.status(200).json({
-        message: 'Successful - PUT by ID',
-        metadata: {
-            id: req.params.id,
-            hostname: req.hostname,
-            method: req.method,
-        },
-    });
-});
-
-// http://localhost:3001/users/34
-router.delete('/:id', (req, res, next) => {
-    res.status(200).json({
-        message: 'Successful - DELETE by ID',
-        metadata: {
-            id: req.params.id,
-            hostname: req.hostname,
-            method: req.method,
-        },
-    });
-});
-
 
 
 module.exports = router;
